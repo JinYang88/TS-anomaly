43d35a9e26519b7864b9d8f2883537199a0b8f15
Jinyang88 <liujy725@gmail.com>
2020-11-05 11:24:20 +0800

    update

--- common/sliding.py
+++ common/sliding.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 class WindowIterator():
     def __init__(self, windows, batch_size, shuffle, num_workers=2):
         self.windows = windows
-        self.loader = DataLoader(windows, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, persistent_workers=True)
+        self.loader = DataLoader(windows, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
     def fetch_windows(self):
         return self.windows
