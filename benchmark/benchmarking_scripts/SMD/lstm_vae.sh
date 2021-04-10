1230aefa3b0f1f5933d3ad17e34d17f31687ac6f
Jinyang88 <liujy725@gmail.com>
2021-04-10 19:10:55 +0800

    update

---
+++ benchmark/benchmarking_scripts/SMD/lstm_vae.sh
@@ -0,0 +1,9 @@
+export PATH=$PATH:/research/dept7/jyliu/cuda/cuda-9.0/bin
+export LD_LIBRARY_PATH=/research/dept7/jyliu/cuda/cuda-9.0/lib64
+
+# python lstm_vae_benchmark.py --dataset SMD --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 32 --stride 5 --hidden_size 128 --num_epochs 10
+
+python lstm_vae_benchmark.py --dataset SMD --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 32 --stride 5 --hidden_size 128 --num_epochs 20
+python lstm_vae_benchmark.py --dataset SMD --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 32 --stride 5 --hidden_size 128 --num_epochs 30
+python lstm_vae_benchmark.py --dataset SMD --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 32 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset SMD --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 32 --stride 5 --hidden_size 128 --num_epochs 10
