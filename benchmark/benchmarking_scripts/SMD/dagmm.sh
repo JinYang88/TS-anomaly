9a841256b8f3305085ff46208e03816b2ba5c9a4
Jinyang88 <liujy725@gmail.com>
2021-04-13 11:30:17 +0800

    update

--- benchmark/benchmarking_scripts/SMD/dagmm.sh
+++ benchmark/benchmarking_scripts/SMD/dagmm.sh
@@ -1,4 +1,4 @@
-python dagmm_benchmark.py --dataset SMD --lr 0.0001 --dropout 0.25 --num_epochs 100 -ch 32 16 2 -eh 80 40
+# python dagmm_benchmark.py --dataset SMD --lr 0.0001 --dropout 0.25 --num_epochs 100 -ch 32 16 2 -eh 80 40
 
 python dagmm_benchmark.py --dataset SMD --lr 0.0001 --dropout 0.25 --num_epochs 100 -ch 32 16 8 -eh 80 40
 
