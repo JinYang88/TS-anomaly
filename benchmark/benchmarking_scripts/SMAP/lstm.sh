01989fecdf839a4f6db656fe44220ccd24f3ac81
Jinyang88 <liujy725@gmail.com>
2021-04-13 16:16:59 +0800

    update

--- benchmark/benchmarking_scripts/SMAP/lstm.sh
+++ benchmark/benchmarking_scripts/SMAP/lstm.sh
@@ -5,30 +5,30 @@ python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 -
 python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 1 --hidden_size 128 --gpu 0
 python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 1 --hidden_size 256 --gpu 0
 
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 32 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 64 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 128 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 256 --gpu 0
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 32 --gpu 1
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 64 --gpu 1
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 128 --gpu 1
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --hidden_size 256 --gpu 1
 
 
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 32 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 64 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 128 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 256 --gpu 0
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 32 --gpu 2
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 64 --gpu 2
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 128 --gpu 2
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 256 --gpu 2
 
 
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 32 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 64 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 128 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 256 --gpu 0
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 32 --gpu 3
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 64 --gpu 3
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 128 --gpu 3
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 1 --hidden_size 256 --gpu 3
 
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 32 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 64 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 128 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 256 --gpu 0
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 32 --gpu 3
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 64 --gpu 3
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 128 --gpu 3
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 2 --hidden_size 256 --gpu 3
 
 
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 32 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 64 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 128 --gpu 0
-python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 256 --gpu 0
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 32 --gpu 4
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 64 --gpu 4
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 128 --gpu 4
+python lstm_benchmark.py --dataset SMAP --lr 0.001 --window_size 64 --stride 5 --num_layers 3 --hidden_size 256 --gpu 4
