0daedbe6e8df9610533b6a92845b085f6cabe917
Jinyang88 <liujy725@gmail.com>
2021-04-13 21:06:43 +0800

    update

---
+++ benchmark/benchmarking_scripts/SMAP/loda.sh
@@ -0,0 +1,9 @@
+python LODA_benchmark.py --dataset SMAP --n_bins 3
+python LODA_benchmark.py --dataset SMAP --n_bins 5
+python LODA_benchmark.py --dataset SMAP --n_bins 10
+python LODA_benchmark.py --dataset SMAP --n_bins 15
+python LODA_benchmark.py --dataset SMAP --n_bins 20
+python LODA_benchmark.py --dataset SMAP --n_bins 25
+python LODA_benchmark.py --dataset SMAP --n_bins 30
+python LODA_benchmark.py --dataset SMAP --n_bins 35
+python LODA_benchmark.py --dataset SMAP --n_bins 40
