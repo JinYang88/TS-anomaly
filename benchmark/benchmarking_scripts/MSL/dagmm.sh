1c5f4c4a1f6156915969225b03c0b42d737f8e3a
Jinyang88 <liujy725@gmail.com>
2021-04-13 14:31:02 +0800

    update

---
+++ benchmark/benchmarking_scripts/MSL/dagmm.sh
@@ -0,0 +1,15 @@
+# sh benchmarking_scripts/MSL/dagmm.sh > logs/dagmm.MSL.bench 2>&1 &
+
+export PATH=$PATH:/research/dept7/jyliu/cuda/cuda-9.0/bin
+export LD_LIBRARY_PATH=/research/dept7/jyliu/cuda/cuda-9.0/lib64
+
+python dagmm_benchmark.py --dataset MSL --lr 0.0001 --dropout 0.25 --num_epochs 100 -ch 32 16 2 -eh 80 40
+python dagmm_benchmark.py --dataset MSL --lr 0.0003 --dropout 0.25 --num_epochs 100 -ch 32 16 2 -eh 80 40
+
+
+python dagmm_benchmark.py --dataset MSL --lr 0.0001 --dropout 0.25 --num_epochs 100 -ch 64 32 2 -eh 60 30
+python dagmm_benchmark.py --dataset MSL --lr 0.0003 --dropout 0.25 --num_epochs 100 -ch 64 32 2 -eh 60 30
+# 
+
+python dagmm_benchmark.py --dataset MSL --lr 0.0001 --dropout 0.25 --num_epochs 100 -ch 128 64 2 -eh 100 50
+python dagmm_benchmark.py --dataset MSL --lr 0.0003 --dropout 0.25 --num_epochs 100 -ch 128 64 2 -eh 100 50
--- benchmark/benchmarking_scripts/SMD/dagmm.sh
+++ benchmark/benchmarking_scripts/SMD/dagmm.sh
@@ -1,5 +1,8 @@
 # sh benchmarking_scripts/SMD/dagmm.sh > logs/dagmm.SMD.bench 2>&1 &
 
+export PATH=$PATH:/research/dept7/jyliu/cuda/cuda-9.0/bin
+export LD_LIBRARY_PATH=/research/dept7/jyliu/cuda/cuda-9.0/lib64
+
 python dagmm_benchmark.py --dataset SMD --lr 0.0001 --dropout 0.25 --num_epochs 100 -ch 32 16 2 -eh 80 40
 python dagmm_benchmark.py --dataset SMD --lr 0.0003 --dropout 0.25 --num_epochs 100 -ch 32 16 2 -eh 80 40
 
