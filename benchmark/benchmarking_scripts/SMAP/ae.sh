fa45d691871b2455456ca12ff0b919c295864f33
Jinyang88 <liujy725@gmail.com>
2021-04-14 15:28:23 +0800

    update

--- benchmark/benchmarking_scripts/SMAP/ae.sh
+++ benchmark/benchmarking_scripts/SMAP/ae.sh
@@ -1,9 +1,7 @@
-python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 128 64 64 128 --batch_size 512 --epochs 100 --l2_regularizer 0.00001
-python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 64 32 32 64 --batch_size 512 --epochs 100 --l2_regularizer 0.00001
+
 python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 32 16 16 32 --batch_size 512 --epochs 100 --l2_regularizer 0.00001
 python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 16 8 8 16  --batch_size 512 --epochs 100 --l2_regularizer 0.00001
 
-python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 128 64 64 128 --batch_size 512 --epochs 100 --l2_regularizer 0.1
-python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 64 32 32 64 --batch_size 512 --epochs 100 --l2_regularizer 0.1
+
 python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 32 16 16 32 --batch_size 512 --epochs 100 --l2_regularizer 0.1
 python AutoEncoder_benchmark.py --dataset SMAP --hidden_neurons 16 8 8 16  --batch_size 512 --epochs 100 --l2_regularizer 0.1
