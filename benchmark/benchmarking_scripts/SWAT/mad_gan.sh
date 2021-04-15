f7daa9a720efba59b478dad8fc4e3dcfac781b0d
Jinyang88 <liujy725@gmail.com>
2021-04-16 00:34:17 +0800

    update

--- benchmark/benchmarking_scripts/SWAT/mad_gan.sh
+++ benchmark/benchmarking_scripts/SWAT/mad_gan.sh
@@ -1,12 +1,12 @@
-python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.005 --num_epochs 50
-python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.005 --num_epochs 150
+CUDA_VISIBLE_DEVICES=1 python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.005 --num_epochs 50
+CUDA_VISIBLE_DEVICES=1 python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.005 --num_epochs 150
 
-python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.005 --num_epochs 50
-python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.005 --num_epochs 150
+CUDA_VISIBLE_DEVICES=1 python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.005 --num_epochs 50
+CUDA_VISIBLE_DEVICES=1 python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.005 --num_epochs 150
 
 
-python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.001 --num_epochs 50
-python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.001 --num_epochs 150
+CUDA_VISIBLE_DEVICES=4 python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.001 --num_epochs 50
+CUDA_VISIBLE_DEVICES=4 python mad_gan_benchmark.py --dataset SWAT --window_size 32 --stride 5 --lr 0.001 --num_epochs 150
 
-python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.001 --num_epochs 50
-python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.001 --num_epochs 150
+CUDA_VISIBLE_DEVICES=4 python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.001 --num_epochs 50
+CUDA_VISIBLE_DEVICES=4 python mad_gan_benchmark.py --dataset SWAT --window_size 64 --stride 5 --lr 0.001 --num_epochs 150
