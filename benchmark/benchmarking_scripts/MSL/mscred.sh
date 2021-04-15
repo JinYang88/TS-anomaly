cee789fcd86d6748bc800968bea7ebff722c03c6
Jinyang88 <liujy725@gmail.com>
2021-04-16 00:24:29 +0800

    update

--- benchmark/benchmarking_scripts/MSL/mscred.sh
+++ benchmark/benchmarking_scripts/MSL/mscred.sh
@@ -1,7 +1,7 @@
-python mscred_benchmark.py --dataset MSL --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 32 --hidden_size 16 --num_epochs 1 --gpu 2
+# python mscred_benchmark.py --dataset MSL --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 32 --hidden_size 16 --num_epochs 1 --gpu 2
 
-python mscred_benchmark.py --dataset MSL --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 64 --hidden_size 32 --num_epochs 1 --gpu 2
+# python mscred_benchmark.py --dataset MSL --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 64 --hidden_size 32 --num_epochs 1 --gpu 2
 
-python mscred_benchmark.py --dataset MSL --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 128 --hidden_size 64 --num_epochs 1 --gpu 3
+# python mscred_benchmark.py --dataset MSL --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 128 --hidden_size 64 --num_epochs 1 --gpu 3
 
 python mscred_benchmark.py --dataset MSL --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 256 --hidden_size 64 --num_epochs 1 --gpu 3
