042c82df0a443857142d4126adb8e61a35cf91ff
Jinyang88 <liujy725@gmail.com>
2021-04-15 22:11:46 +0800

    update

--- benchmark/benchmarking_scripts/WADI/lstm_vae.sh
+++ benchmark/benchmarking_scripts/WADI/lstm_vae.sh
@@ -15,31 +15,31 @@ python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 4 --intermediate_
 
 # window_size, 64
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 1 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 1 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 2 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 2 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 4 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 4 --intermediate_dim 64 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
 
-# window_size, 128
+# # window_size, 128
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 1 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 1 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 2 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 2 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 3 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 4 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 4 --intermediate_dim 64 --window_size 128 --stride 5 --hidden_size 128 --num_epochs 40
 
 
-# intermediate_dim 32
+# # intermediate_dim 32
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 1 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 1 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 2 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 2 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 3 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 3 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
 
-python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 4 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
+# python lstm_vae_benchmark.py --dataset WADI --lr 0.001 --z_dim 4 --intermediate_dim 32 --window_size 64 --stride 5 --hidden_size 128 --num_epochs 40
