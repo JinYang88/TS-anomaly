98b3b08d5d9e935452fb4a79ef5dd1b985dd9738
Jinyang88 <liujy725@gmail.com>
2021-04-17 10:36:39 +0800

    update

--- benchmark/benchmarking_scripts/SMAP/mscred.sh
+++ benchmark/benchmarking_scripts/SMAP/mscred.sh
@@ -4,4 +4,4 @@
 
 # python mscred_benchmark.py --dataset SMAP --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 128 --hidden_size 64 --num_epochs 1 --gpu 5
 
-python mscred_benchmark.py --dataset SMAP --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 256 --num_epochs 3 --gpu 5
+python mscred_benchmark.py --dataset SMAP --lr 0.001 --in_channels_encoder 3 --in_channels_decoder 256 --num_epochs 1 --gpu 2
