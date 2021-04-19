e4deb0899a9e024485c62c077fc60f686852d913
Jinyang88 <liujy725@gmail.com>
2021-04-19 16:43:00 +0800

    update

--- benchmark/benchmarking_scripts/BEST/cpu.sh
+++ benchmark/benchmarking_scripts/BEST/cpu.sh
@@ -6,7 +6,7 @@ python iforest_benchmark.py --dataset WADI --n_estimators 64
 python KNN_benchmark.py --dataset MSL --n_neighbors 5
 python KNN_benchmark.py --dataset SMAP --n_neighbors 20
 python KNN_benchmark.py --dataset SMD --n_neighbors 30
-python KNN_benchmark.py --dataset SWAT_SPLIT --n_neighbors 30
+python KNN_benchmark.py --dataset SWAT --n_neighbors 30
 python KNN_benchmark.py --dataset WADI_SPLIT --n_neighbors 3
 python LODA_benchmark.py --dataset MSL --n_bins 40
 python LODA_benchmark.py --dataset SMAP --n_bins 30
@@ -16,7 +16,7 @@ python LODA_benchmark.py --dataset WADI --n_bins 40
 python LOF_benchmark.py --dataset MSL --n_neighbors 5
 python LOF_benchmark.py --dataset SMAP --n_neighbors 10
 python LOF_benchmark.py --dataset SMD --n_neighbors 30
-python LOF_benchmark.py --dataset SWAT_SPLIT --n_neighbors 3
+python LOF_benchmark.py --dataset SWAT --n_neighbors 3
 python LOF_benchmark.py --dataset WADI_SPLIT --n_neighbors 3
 python PCA_benchmark.py --dataset MSL --n_components 25
 python PCA_benchmark.py --dataset SMAP --n_components 10
