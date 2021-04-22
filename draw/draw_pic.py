3d6bc3f3350b6e1719ea4157ccb0e174d9081929
辜文蔚 <1743871503@qq.com>
2021-04-22 23:48:00 +0800

    update bar plot

--- draw/draw_pic.py
+++ draw/draw_pic.py
@@ -21,20 +21,21 @@ width = 0.35       # the width of the bars: can also be len(x) sequence
 fig, ax = plt.subplots()
 
 ax.set_yscale('symlog')
-ax.bar(model, train_time, width, label='Train time')
-ax.bar(model, train_time * test_time, width, bottom=train_time,
-       label='Test Time')
+x = np.arange(len(model))
+plt.xticks(x, model)
+ax.bar(x - width / 2, train_time, width, label='Train time')
+ax.bar(x + width / 2, test_time, width, label='Test Time')
 
 
 def convert_unit(data_list):
     convert = []
     for i in range(len(data_list)):
-        if data_list[i] > 3600:
-            convert.append(str(round(data_list[i] // 60, 2)) + 'min')
-        elif data_list[i] > 86400:
-            convert.append(str(round(data_list[i] // 3600, 2)) + 'h')
+        if data_list[i] > 60000:
+            convert.append(str(int(data_list[i] / 3600)) + 'h')
+        elif 1000 < data_list[i] < 60000:
+            convert.append(str(int(data_list[i] / 60)) + 'min')
         else:
-            convert.append(str(round(data_list[i], 2)) + 's')
+            convert.append(str(int(data_list[i])) + 's')
 
     return convert
 
@@ -44,11 +45,11 @@ test_convert = convert_unit(test_time)
 
 train_value = [round(i, 3) for i in train_time]
 for i in range(len(train_value)):
-    plt.text(x=i-0.2, y=train_value[i], s=train_convert[i], size=15)
+    plt.text(x=i - width, y=train_value[i], s=train_convert[i], size=15)
 
 test_value = [round(i, 3) for i in test_time]
 for i in range(len(test_value)):
-    plt.text(x=i-0.2, y=test_value[i]*train_value[i]+train_value[i], s=test_convert[i], size=15)
+    plt.text(x=i, y=test_value[i], s=test_convert[i], size=15)
 
 
 ax.set_ylabel('Time')
