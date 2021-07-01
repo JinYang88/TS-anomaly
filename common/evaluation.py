952a0b71bafaae94929b1d4d161685d00c6eb108
jinyang88 <liujy725@gmail.com>
2021-07-01 14:42:58 +0800

    update

--- common/evaluation.py
+++ common/evaluation.py
@@ -21,7 +21,7 @@ metric_func = {
 def json_pretty_dump(obj, filename):
     with open(filename, "w") as fw:
         json.dump(
-            {k: float(v) for k,v in obj.items()},
+            {k: str(v) for k,v in obj.items()},
             fw,
             sort_keys=True,
             indent=4,
