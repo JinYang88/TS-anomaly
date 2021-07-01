7358508dd540bb200d82561fd8fb8be8dca784dd
jinyang88 <liujy725@gmail.com>
2021-07-01 14:33:43 +0800

    update

---
+++ benchmark/cmanomaly_benchmark.py
@@ -0,0 +1,134 @@
+import sys
+
+sys.path.append("../")
+
+import os
+import hashlib
+import traceback
+from common import data_preprocess
+from common.dataloader import load_dataset
+from common.batching import WindowIterator
+from common.utils import print_to_json, update_from_nni_params, seed_everything, pprint
+from networks.cmanomaly import CMAnomaly
+
+import argparse
+from common.config import subdatasets
+from common.evaluation import (
+    store_benchmarking_results,
+    evaluate_benchmarking_folder,
+)
+
+seed_everything(2020)
+
+
+# python cmanomaly_benchmark.py --dataset SMD --lr 0.001 --window_size 32 --stride 5 --num_layers 2 --embedding_dim 64 --gpu 0
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", type=str, help="Dataset used")
+
+parser.add_argument("--lr", type=float, help="learning rate")
+parser.add_argument("--window_size", type=int, help="window_size")
+parser.add_argument("--stride", type=int, help="stride")
+parser.add_argument("--num_layers", type=int, help="num_layers")
+parser.add_argument("--embedding_dim", type=int, help="embedding_dim")
+parser.add_argument("--gpu", type=int, default=0, help="The gpu index, -1 for cpu")
+
+args = vars(parser.parse_args())
+
+model_name = "lstm"  # change this name for different models
+benchmarking_dir = "./benchmarking_results"
+hash_id = hashlib.md5(
+    str(sorted([(k, v) for k, v in args.items()])).encode("utf-8")
+).hexdigest()[0:8]
+
+dataset = args["dataset"]
+device = args["gpu"]
+window_size = args["window_size"]
+stride = args["stride"]
+lr = args["lr"]
+embedding_dim = args["embedding_dim"]
+num_layers = args["num_layers"]
+
+
+normalize = "minmax"
+nb_epoch = 1000
+patience = 5
+dropout = 0
+batch_size = 1024
+prediction_length = 1
+prediction_dims = []
+
+if __name__ == "__main__":
+    for subdataset in subdatasets[dataset][0:2]:
+        try:
+            save_path = os.path.join("./savd_dir_lstm", hash_id, subdataset)
+
+            print(f"Running on {subdataset} of {dataset}")
+            data_dict = load_dataset(dataset, subdataset, "all", root_dir="../")
+
+            pp = data_preprocess.preprocessor()
+            data_dict = pp.normalize(data_dict, method=normalize)
+            os.makedirs(save_path, exist_ok=True)
+            pp.save(save_path)
+
+            window_dict = data_preprocess.generate_windows(
+                data_dict,
+                window_size=window_size,
+                stride=stride,
+            )
+
+            train_iterator = WindowIterator(
+                window_dict["train_windows"], batch_size=batch_size, shuffle=True
+            )
+            test_iterator = WindowIterator(
+                window_dict["test_windows"], batch_size=4096, shuffle=False
+            )
+
+            encoder = CMAnomaly(
+                in_channels=data_dict["dim"],
+                window_size=window_size,
+                embedding_dim=embedding_dim,
+                dropout=dropout,
+                prediction_length=prediction_length,
+                prediction_dims=prediction_dims,
+                patience=patience,
+                save_path=save_path,
+                batch_size=batch_size,
+                nb_epoch=nb_epoch,
+                lr=lr,
+                device=device,
+            )
+
+            encoder.fit(
+                train_iterator,
+                test_iterator=test_iterator.loader,
+                test_labels=window_dict["test_labels"],
+            )
+
+            encoder.load_encoder()
+            records = encoder.predict_prob(test_iterator.loader)
+
+            records_train = encoder.predict_prob(train_iterator.loader)
+
+            train_anomaly_score = records_train["score"]
+            anomaly_score = records["score"]
+            anomaly_label = window_dict["test_labels"][:, -1]
+
+            eval_folder = store_benchmarking_results(
+                hash_id,
+                benchmarking_dir,
+                dataset,
+                subdataset,
+                args,
+                model_name,
+                {"train": train_anomaly_score, "test": anomaly_score},
+                anomaly_label,
+                encoder.time_tracker,
+            )
+        except Exception as e:
+            print(f"Running on {subdataset} failed.")
+            print(traceback.format_exc())
+
+    average_monitor_metric = evaluate_benchmarking_folder(
+        eval_folder, benchmarking_dir, hash_id, dataset, model_name
+    )
---
+++ networks/cmanomaly/cmanomaly.py
@@ -0,0 +1,148 @@
+## Unit test only start
+# import torch
+# import sys
+import torch
+import itertools
+import torch.nn.functional as F
+from IPython import embed
+from torch import nn
+from networks.wrappers import TimeSeriesEncoder
+
+
+class AFMLayer(torch.nn.Module):
+    def __init__(self, embed_dim, attn_size=32, dropout=0.1):
+        super().__init__()
+        self.attention = torch.nn.Linear(embed_dim, attn_size)
+        self.projection = torch.nn.Linear(attn_size, 1)
+        self.fc = torch.nn.Linear(embed_dim, 1)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        :param x: Float tensor of size ``(batch_size, num_fields, embed_dim)``
+        """
+        num_fields = x.shape[1]
+        row, col = list(), list()
+        for i in range(num_fields - 1):
+            for j in range(i + 1, num_fields):
+                row.append(i), col.append(j)
+        p, q = x[:, row], x[:, col]
+        inner_product = p * q
+
+        attn_scores = F.relu(self.attention(inner_product))
+        attn_scores = F.softmax(self.projection(attn_scores), dim=1)
+        attn_scores = self.dropout(attn_scores)
+        attn_output = torch.sum(attn_scores * inner_product, dim=1)
+        attn_output = self.dropout(attn_output)
+        return attn_output
+
+
+class CMAnomaly(TimeSeriesEncoder):
+    """
+    Encoder of a time series using a LSTM, ccomputing a linear transformation
+    of the output of an LSTM
+
+    Takes as input a three-dimensional tensor (`B`, `L`, `C`) where `B` is the
+    batch size, `C` is the number of input channels, and `L` is the length of
+    the input. Outputs a two-dimensional tensor (`B`, `C`).
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        window_size,
+        vocab_size=None,
+        embedding_dim=None,
+        dropout=0,
+        prediction_length=1,
+        prediction_dims=[],
+        gamma=0.01,
+        **kwargs,
+    ):
+        super().__init__(architecture="CMAomaly", **kwargs)
+
+        self.prediction_dims = (
+            prediction_dims if prediction_dims else list(range(in_channels))
+        )
+        self.prediction_length = prediction_length
+        self.gamma = gamma
+
+        if vocab_size is not None and embedding_dim is not None:
+            self.embedder = nn.Embedding(vocab_size, embedding_dim)
+            lstm_input_dim = embedding_dim
+        else:
+            self.embedder = None
+            lstm_input_dim = in_channels
+
+        final_output_dim = prediction_length * len(self.prediction_dims)
+
+        clf_input_dim = in_channels + window_size - 1
+
+        self.time_afm = AFMLayer(in_channels)
+        self.feat_afm = AFMLayer(window_size - 1)
+
+        self.res_w = nn.Linear(
+            in_channels * (window_size - 1),
+            window_size - 1 + in_channels,
+        )
+
+        self.linear = nn.Sequential(
+            nn.Linear(clf_input_dim, 128),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(64, final_output_dim),
+        )
+
+        # self.linear = nn.Sequential(nn.Linear(clf_input_dim, final_output_dim))
+
+        self.dropout = nn.Dropout(dropout)
+        self.loss_fn = nn.MSELoss(reduction="none")
+
+        self.compile()
+
+    def CM_interaction(self, x):
+        """
+        shape of x: (batchsize, dims, feature lens)
+        """
+        sum_of_square = torch.sum(x, dim=1) ** 2
+        square_of_sum = torch.sum(x ** 2, dim=1)
+        bi_interaction_vector = (sum_of_square - square_of_sum) * 0.5
+        # return bi_interaction_vector.sum(dim=-1, keepdim=True)
+        return bi_interaction_vector
+
+    def forward(self, batch_window):
+        # batch_window = batch_window.permute(0, 2, 1)  # b x win x ts_dim
+        self.batch_size = batch_window.size(0)
+        x, y = (
+            batch_window[:, 0 : -self.prediction_length, :],
+            batch_window[:, -self.prediction_length :, self.prediction_dims],
+        )
+
+        time_inter = self.time_afm(x)
+        dim_inter = self.feat_afm(x.transpose(2, 1))
+
+        interactions = torch.cat([time_inter, dim_inter], dim=-1)
+        outputs = self.dropout(interactions)
+        recst = self.linear(outputs).view(
+            self.batch_size, self.prediction_length, len(self.prediction_dims)
+        )
+
+        loss = self.loss_fn(recst, y)
+        return_dict = {
+            "loss": loss.sum(),
+            "recst": recst,
+            "repr": outputs,
+            "score": loss,
+            "y": y,
+        }
+
+        return return_dict
+
+
+if __name__ == "__main__":
+    inp = torch.randn((32, 1, 46))
+    model = MultiLSTM(in_channels=25, num_layers=1, window_size=45)
+    out = model.forward(inp)
