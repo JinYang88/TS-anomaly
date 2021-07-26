8dba784e33e1a4d20926cef2036240ca9ff2cd08
jinyang88 <liujy725@gmail.com>
2021-07-26 23:28:44 +0800

    update

--- common/batching.py
+++ common/batching.py
@@ -4,6 +4,13 @@ import numpy as np
 from IPython import embed
 from torch.utils.data import Dataset
 
+from sklearn.preprocessing import MultiLabelBinarizer
+
+test = pd.Series([["a", "b", "e"], ["c", "a"], ["d"], ["d"], ["e"]])
+
+mlb = MultiLabelBinarizer()
+res = pd.DataFrame(mlb.fit_transform(test), columns=mlb.classes_, index=test.index)
+
 
 class WindowIterator:
     def __init__(self, windows, batch_size, shuffle, num_workers=2):
@@ -25,9 +32,12 @@ class TokenDataset(Dataset):
         return len(self.windows)
 
     def __getitem__(self, idx):
+        y = torch.zeros(26)
+        y_indice = list(
+            map(lambda x: self.vocab.label2idx[x], self.windows[idx, -1, :])
+        )
+        y[y_indice] = 1
         return {
             "x": self.windows[idx, :-1, :],
-            "y": torch.LongTensor(
-                list(map(lambda x: self.vocab.label2idx[x], self.windows[idx, -1, :]))
-            ),
+            "y": torch.LongTensor(y),
         }
