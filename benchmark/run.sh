69d8c32a27f9b815f332d4e7b42f209239962e86
jinyangliu <liujy725@gmail.com>
2021-08-02 19:30:34 +0800

    update

---
+++ benchmark/run.sh
@@ -0,0 +1,17 @@
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 64 --stride 5 --embedding_dim 16 --nbins 5 --gpu 0 B
+
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 64 --stride 5 --embedding_dim 16 --nbins 8 --gpu 0 B
+
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 64 --stride 5 --embedding_dim 16 --nbins 10 --gpu 1 B
+
+
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 64 --stride 5 --embedding_dim 32 --nbins 10 --gpu 1 B
+
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 64 --stride 5 --embedding_dim 64 --nbins 10 --gpu 2 B
+
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 64 --stride 5 --embedding_dim 8 --nbins 10 --gpu 2 B
+
+
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 8 --stride 5 --embedding_dim 16 --nbins 10 --gpu 3 B
+
+python cmanomaly_benchmark.py --dataset SMD --lr 0.01 --window_size 128 --stride 5 --embedding_dim 16 --nbins 10 --gpu 3 B
